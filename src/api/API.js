f3e0f3770141a77cef4c59dbd7164309c81eb948
Charles-Antoine LEGER <toutane.leger@gmail.com>
2018-08-06 12:02:20 +0200

    setup backend url api

--- src/api/API.js
+++ src/api/API.js
@@ -1,6 +1,6 @@
 // Readable fetchs API
-const api = "http://localhost:3001";
-// const api = "https://focused-pike-e9496d.netlify.com";
+// const api = "http://localhost:3001";
+const api = "https://hangman-game-be-wlicrjynhn.now.sh/";
 
 // fetch projects
 export const getWords = difficulty =>
