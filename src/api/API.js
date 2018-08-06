c28f42bad8be298866b148e5d505ead6d65ddf14
Charles-Antoine LEGER <toutane.leger@gmail.com>
2018-08-06 10:42:19 +0200

    add netlify backend api

--- src/api/API.js
+++ src/api/API.js
@@ -1,6 +1,6 @@
 // Readable fetchs API
 // const api = "http://localhost:3001";
-const api = "https://cranky-heisenberg-af574b.netlify.com/";
+const api = "https://focused-pike-e9496d.netlify.com/";
 
 // fetch projects
 export const getWords = difficulty =>
